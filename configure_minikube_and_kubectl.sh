2b48b646fede7c577209fd89fa517799298220b9
Suheab1995 <124862610+Suheab1995@users.noreply.github.com>
2024-05-18 13:46:53 +0530

    Create configure_minikube_and_kubectl.sh

---
+++ configure_minikube_and_kubectl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install Minikube
+sudo apt-get install -y conntrack
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Start Minikube
+minikube start --driver=none
+
+# Verify Minikube status
+minikube status
+
+# Enable Ingress addon
+minikube addons enable ingress
